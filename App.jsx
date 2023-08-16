3dca8e226c0fad8c4b6fe43a6549cfabcd167ba5
sulekha1234 <sulakhe1234@gmail.com>
2023-08-16 18:27:29 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import StopWatch from './component/stopwatch/Stopwatch'
+import "./app.css"
+
+const App = () => {
+  return (
+    <div className='app'>
+      <StopWatch />
+    </div>
+  )
+}
+
+export default App
